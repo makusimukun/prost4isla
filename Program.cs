31657413205baff23fbd9b31fbbfcf3174170fe1
unknown <maksimatveev92@mail.ru>
2023-03-15 13:49:04 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+bool isPrime = true;
+for (int i = 2; i < number; i++)
+{
+    if (number % i == 0)
+    {
+        isPrime = false;
+        break;
+    }
+}
+if (isPrime)
+{
+    Console.WriteLine($"{number} - простое число");
+}
+else
+{
+    Console.WriteLine($"{number} - не простое число");
+}
